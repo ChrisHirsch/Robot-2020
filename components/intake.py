34c16ef3ce18bf32ab999e6ad977ef98938c49ab
ManiacMoustache <43834579+ManiacMoustache@users.noreply.github.com>
2020-02-15 11:51:57 -0700

    Small bits intake, not implemented in robot.py

--- components/intake.py
+++ components/intake.py
@@ -1,3 +1,22 @@
 import motorHelper
 
-class Intake
+
+class Intake:
+    # Note - The way we will want to do this will be to give this component motor description dictionaries from robotmap and then creating the motors with motorhelper. After that, we simply call wpilib' differential drive
+    intake_motorsList: dict
+
+    def setup(self):
+        self.motors = {}
+        self.speed = 0
+        for motorDescKey in self.intake_motorsList:
+            currentMotor = self.intake_motorsList[motorDescKey]
+            print("{}".format(currentMotor))
+            self.motors[motorDescKey] = motorHelper.createMotor(currentMotor)
+
+        self.shooterMotor = self.motors['shooterMotor']
+    
+    def setSpeed(self, speed):
+        self.speed = speed
+
+    def execute(self):
+        self.shooterMotor.set(self.speed)
